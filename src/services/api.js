021e2e66d0c7a7c3b66b41bc14295e1649c3ea17
Huân Nè <Huannek2006@gmail.com>
2025-06-17 15:32:34 +0700

    fix lai crud va endpoint pgg

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiService = axios.create({
-    baseURL: 'http://localhost:8080/api', // Kiểm tra port
+    baseURL: 'http://localhost:8080', 
     headers: {
         'Content-Type': 'application/json',
     },
