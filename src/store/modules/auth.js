0c67809c129362f92ac9b62896841eaeb310e6a4
NgVietKhoa <121367292+NgVietKhoa@users.noreply.github.com>
2025-07-30 18:53:04 +0700

    Update auth.js

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    isAuthenticated: true, 
+    isAuthenticated: false, 
     user: null,
   }),
   actions: {
