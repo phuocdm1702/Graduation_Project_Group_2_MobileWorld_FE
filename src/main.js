469fb33abf8ee7eecae1b99e1fe80bc433c3839a
Nguyen Viet Khoa <khoa2006nguyen811@gmail.com>
2025-08-31 21:51:56 +0700

    sua chuot thanh hinh dien thoai

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import pinia from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import './style.css';
 
 const app = createApp(App);
 app.use(pinia);
